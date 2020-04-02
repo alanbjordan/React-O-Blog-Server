3bd4263f003e0d6355da31e97480410b5b452bee
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-02 14:55:41 -0400

    removed routers to debug

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const singlePostRouter = require('./routes/blogs');
+// const singlePostRouter = require('./routes/blogs');
 
 const app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/blogs', singlePostRouter);
+// app.use('/blogs', singlePostRouter);
 
 module.exports = app;
