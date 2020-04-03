d51d6666ad773e70a1f3a76426f7928a1fc5371c
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-03 14:53:56 -0400

    Clean Up -Removed comments -Removed console logs

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const blogsModel = require('../models/blogsModel');
 
-/* GET home page. */
 router.get('/', async function(req, res, next) {
   const resultData = await blogsModel.getAllBlogs();
-  console.log(resultData);
   res.json(resultData).status(200)
 });
 
